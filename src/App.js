e50de8f65498283dcc8ee3ba6a1a55fdccd6c6a7
antoniojunioalves <antoniojunioalves@gmail.com>
2019-09-17 18:05:35 -0300

    Implementando a busca na API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  state = {
+    jsonInicial: {}
+  }
+
+  componentDidMount() {
+    fetch('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=64303698a48effb62677752107150d355a274937')
+      .then(response => {
+        if (!response.ok)
+          throw new Error()
+
+        return response.json()
+      })
+      // .then(response => this.setState({ ...this.state, jsonInicial: response }))
+      .then(response => console.log(response))
+      .catch((error) => console.log('3', error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Criptografia de Júlio César
+          </p>
+          <div>
+            {this.state.jsonInicial.map(j => { <p>{j.cifrado}</p> })}
+          </div>
+          <p>
+            Texto decifrado
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
