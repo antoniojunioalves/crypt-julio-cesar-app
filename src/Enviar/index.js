13b94de054247a865d8b954777f0dd2c93466672
antoniojunioalves <antoniojunioalves@gmail.com>
2019-09-26 19:05:06 -0300

    Aplicação Finalizada para enviar o arquivo. Ajustar Layout Futuramente.

--- src/Enviar/index.js
+++ src/Enviar/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import axios from 'axios';
 
-// Exemplo: https://www.techiediaries.com/react-formdata-file-upload-multipart-form-tutorial/
 class FileUploadForm extends React.Component {
   UPLOAD_ENDPOINT = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=64303698a48effb62677752107150d355a274937'
+  // UPLOAD_ENDPOINT = 'https://hookb.in/oXojYGNGJptWlWL0Ox0g'
   constructor(props) {
     super(props);
     this.state = {
       file: null
     }
+
     this.onSubmit = this.onSubmit.bind(this)
     this.onChange = this.onChange.bind(this)
     this.uploadFile = this.uploadFile.bind(this)
@@ -16,18 +17,15 @@ class FileUploadForm extends React.Component {
   async onSubmit(e) {
     e.preventDefault()
     let res = await this.uploadFile(this.state.file);
-    console.log(res.data);
   }
 
   onChange(e) {
-    console.log(e.target.files)
     this.setState({ file: e.target.files[0] })
   }
 
   async uploadFile(file) {
-    console.log(file)
     const formData = new FormData();
-    formData.append(file.file, file.name);
+    formData.append('answer', file);
 
     // return await fetch(this.UPLOAD_ENDPOINT, {
     //   method: 'POST',
@@ -44,8 +42,9 @@ class FileUploadForm extends React.Component {
 
   render() {
     return (
+      // <form onSubmit={this.onSubmit} encType="multipart/form-data">
       <form onSubmit={this.onSubmit}>
-        <h1> React File Upload Example</h1>
+        <h1> React File Upload</h1>
         <input type="file" onChange={this.onChange} />
         <button type="submit">Upload File</button>
       </form>
